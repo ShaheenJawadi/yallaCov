e5659166a90950d37dbe29dc74aa2f5e70cc0792
shaheenj <jawadishaheen@gmail.com>
2024-12-20 18:25:14 +0100

    Create Review Entity and Repository

---
+++ src/Entity/Review.php
@@ -0,0 +1,56 @@
+<?php
+// src/Entity/Review.php
+namespace App\Entity;
+
+use App\Repository\ReviewRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReviewRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Review
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+
+    #[ORM\Column]
+    private ?int $rating = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $comment = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(?int $rating): void
+    {
+        $this->rating = $rating;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): void
+    {
+        $this->comment = $comment;
+    }
+
+
+}
