1ef4e5a0f8c8605d60c9810e982b8f0aaa972581
shaheenj <jawadishaheen@gmail.com>
2024-12-21 07:14:39 +0100

    Feat : Add listing page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request; 
+use App\Repository\RideRepository;
 
 class HomeController extends AbstractController
 {
@@ -13,4 +15,22 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig');
     }
+
+    #[Route('/list', name: 'app_ride_listing')]
+    public function list(Request $request, RideRepository $rideRepository): Response
+    {
+        $departureCity = $request->query->get('departureCity');
+        $arrivalCity = $request->query->get('arrivalCity');
+        $departureDate = $request->query->get('departureDate');
+        $departureTime = $request->query->get('departureTime');
+
+        
+        $rides = $rideRepository->findByCriteria($departureCity, $arrivalCity, $departureDate, $departureTime);
+
+        return $this->render('home/list.html.twig', [
+            'rides' => $rides,
+        ]);
+ 
+    }
+
 }
